044b7770a9f9dd1ed45b032aa4ac4cd56d2bd03d
jaymalave <jaym.github@gmail.com>
2021-07-13 23:59:06 +0530

    added stream-builders

--- LoginBLOC/login_bloc/lib/src/screens/login_screen.dart
+++ LoginBLOC/login_bloc/lib/src/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import 'dart:async';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
@@ -16,24 +18,43 @@ class LoginScreen extends StatelessWidget {
   }
 
   Widget emailField() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-          alignLabelWithHint: true,
-          hintText: 'you@example.com',
-          labelText: 'Email Address'),
-    );
-  }
+
+    //Stream Builders
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot){
+            return TextField(
+              onChanged: bloc.changeEmail,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  alignLabelWithHint: true,
+                  hintText: 'you@example.com',
+                  labelText: 'Email Address',
+                  errorText: snapshot.error.toString(),
+               ),
+             );
+           }
+        );
+      }
 
   Widget passwordField() {
-    return TextField(
+   return StreamBuilder(
+     stream: bloc.password,
+     builder: (context, snapshot){
+           return TextField(
       obscureText: true,
       decoration: InputDecoration(
         alignLabelWithHint: true,
         hintText: 'Password',
         labelText: 'Password',
+        errorText: snapshot.error.toString(),
       ),
     );
+  },
+   );
+
+
+    
   }
 
   Widget submitButton() {
