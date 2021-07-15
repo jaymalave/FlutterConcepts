45c9962c69a13fabbd3f7c294d35ee09ed45b0c0
jaymalave <jaym.github@gmail.com>
2021-07-16 02:20:43 +0530

    added combinelatest function from rxdart

--- LoginBLOC/login_bloc/lib/src/screens/login_screen.dart
+++ LoginBLOC/login_bloc/lib/src/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
 import '../blocs/provider.dart';
@@ -5,65 +7,73 @@ import 'dart:async';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
-    
     final bloc = Provider.of(context);
 
     return Container(
       margin: EdgeInsets.all(40.0),
       child: Column(
         children: [
-          emailField(bloc), 
+          emailField(bloc),
           passwordField(bloc),
-          Container(margin: EdgeInsets.only(top: 25.0),),
-          submitButton(),
-          ],
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
+          ),
+          submitButton(bloc),
+        ],
       ),
     );
   }
 
   Widget emailField(Bloc bloc) {
-
     //Stream Builders
     return StreamBuilder(
-      stream: bloc.email,
-      builder: (context, snapshot){
-            return TextField(
-              onChanged: bloc.changeEmail,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  alignLabelWithHint: true,
-                  hintText: 'you@example.com',
-                  labelText: 'Email Address',
-                  errorText: snapshot.error?.toString()??"",
-               ),
-             );
-           }
-        );
-      }
-
-  Widget passwordField(Bloc bloc) {
-   return StreamBuilder(
-     stream: bloc.password,
-     builder: (context, snapshot){
-           return TextField(
-      onChanged: bloc.changePassword,
-      obscureText: true,
-      decoration: InputDecoration(
-        alignLabelWithHint: true,
-        hintText: 'Password',
-        labelText: 'Password',
-        errorText: snapshot.error?.toString()??"",
-      ),
-    );
-  },
-   );
+        stream: bloc.email,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              alignLabelWithHint: true,
+              hintText: 'you@example.com',
+              labelText: 'Email Address',
+              errorText: snapshot.error?.toString() ?? "",
+            ),
+          );
+        });
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      onPressed: () {},
-      child: Text('Log in'),
-      color: Colors.blue,
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            alignLabelWithHint: true,
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error?.toString() ?? "",
+          ),
+        );
+      },
+    );
+  }
+
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          onPressed: snapshot.hasError
+              ? null
+              : () {
+                  print('Enabled!');
+                },
+          child: Text('Log in'),
+          color: Colors.blue,
+        );
+      },
     );
   }
 }
